70ca89c851630b0a7d01e48d9c550a6114923aa6
akash <akashvj98@gmail.com>
2023-07-12 10:53:38 -0500

    Same binary tree

---
+++ LeetCode/SameBinaryTree.java
@@ -0,0 +1,55 @@
+package LeetCode;
+
+public class SameBinaryTree {
+
+      public class TreeNode {
+         int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+
+
+
+            return dfs(p,q);
+
+        }
+
+        boolean dfs(TreeNode p, TreeNode q){
+
+            if(p==null && q==null){
+                return true;
+            }
+
+            if(p == null || q== null || p.val != q.val){
+
+                return false;
+            }
+
+
+
+
+
+
+            boolean left =  dfs(p.left,q.left) ;
+            boolean right = dfs(p.right,q.right);
+
+            if(left == false|| right==false){
+                return false;
+            }
+            return true;
+
+
+
+        }
+
+}
