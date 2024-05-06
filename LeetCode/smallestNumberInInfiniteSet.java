a8ca0e005f6755fc393a11ad464b2c0937dc710b
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-05-06 13:25:22 -0700

    Update smallestNumberInInfiniteSet.java

--- LeetCode/smallestNumberInInfiniteSet.java
+++ LeetCode/smallestNumberInInfiniteSet.java
@@ -38,3 +38,18 @@ class SmallestInfiniteSet {
  * int param_1 = obj.popSmallest();
  * obj.addBack(num);
  */
+
+/** Time complexity:
+
+O((m+n)⋅log⁡n)O((m + n) \cdot \log n)O((m+n)⋅logn)
+
+In each popSmallest() method call, in the worst case, we will need to remove a number from the hash set which will take O(1)O(1)O(1) time, and the top of the min-heap which will take O(log⁡n)O(\log n)O(logn) time. Thus, for mmm calls it will take O(m⋅log⁡n)O(m \cdot \log n)O(m⋅logn) time.
+
+In each addBack(num) method call, we might push num in the hash set which will take O(1)O(1)O(1) time and min-heap which will take O(log⁡n)O(\log n)O(logn) time. Thus, for nnn calls it will take O(n⋅log⁡n)O(n \cdot \log n)O(n⋅logn) time.
+
+Space complexity:
+
+O(n)O(n)O(n)
+
+In the worst case, we might add nnn elements in the hash set and the min-heap. Thus, it will take O(n)O(n)O(n) space.
+*/
