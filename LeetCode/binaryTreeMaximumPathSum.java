a19e3f35151b076fc7dd20dbeb499ec05945b3a7
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-05-07 17:03:26 -0700

    Update binaryTreeMaximumPathSum.java

--- LeetCode/binaryTreeMaximumPathSum.java
+++ LeetCode/binaryTreeMaximumPathSum.java
@@ -13,6 +13,33 @@
  *     }
  * }
  */
+
+/* 
+In this problem, we try to find the maximum path from adjacent nodes.
+so  lets take example of binary tree:                       
+
+                        -10
+                        / \
+                      9    20
+                           / \
+                        15     7
+          we will start from the left bottom, so in the begining max will be 9.
+          then max will be 15+20+7 = 42.
+          finaly we check for 9+ -10 + 20, since it is less than 42 our max will be 42.
+
+                        10
+                        / \
+                      1    20
+                           / \
+                        15     7
+
+                here first max will be 10(root value).
+                then max will be 15+20+7 = 42.
+                but to root value 10, function calls dfs(root.left) will return 1 and dfs(root.right) will return 35.
+                since 10+35+1=46 will be greater than 42, max will be 46.
+                
+          
+*/
 class Solution {
     int max= 0;
     public int maxPathSum(TreeNode root) {
