2bd5dd075199505e7b84f09cc81fe84dc13e2fc2
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-05-22 11:20:12 -0700

    Update bestTimeToBuyAndSellStock.java

--- LeetCode/bestTimeToBuyAndSellStock.java
+++ LeetCode/bestTimeToBuyAndSellStock.java
@@ -3,11 +3,15 @@ class Solution {
         int minBuy = Integer.MAX_VALUE;
         int maxProfit = 0;
         for(int i= 0;i<prices.length;i++){
+            //find the minimum value from the array
+
+            //If there are no other elements greater than the min element for example[7,3,4,5,1] 1 in this case, dont need to worry because we already have a maxProfit(5-3).
             if(prices[i]<minBuy){
                 minBuy=prices[i];
                 
             }
             else{
+                // we have already found a minvalue, so we just minus it from the other elements of the array to find the max profit.
                 maxProfit=Math.max(maxProfit, prices[i]-minBuy);
             }
         }
