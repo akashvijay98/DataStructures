6413784e5d47dddb75c5d082cde47827248f44a3
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-06-13 22:57:14 -0700

    Update taskScheduler.java

--- LeetCode/taskScheduler.java
+++ LeetCode/taskScheduler.java
@@ -1,3 +1,10 @@
+/*
+    take example: [A,A,A,B,B,B,C,C,C], n=3
+    cycle =4
+    
+    so A will remain idle for next 3 cycles --> A, B, C, idle, A, B, C, idle, A, B, C 
+*/
+
 class Solution {
     public int leastInterval(char[] tasks, int n) {
 
@@ -26,6 +33,10 @@ class Solution {
             int cycle = n+1;
             int taskCount=0;
 
+            // cycle-->0: This part of the condition ensures that the inner loop runs for 
+            //a maximum of n + 1 iterations.
+
+
             while(cycle-->0 && !pq.isEmpty()){
                 
                 int currentFreq = pq.poll();
@@ -41,6 +52,9 @@ class Solution {
                 pq.offer(freq);
             }
 
+            // for example if input is AABC and n=2, then in the first iteration, A,B,C Tasks will get executed,
+            // so time for first iteration will be n+1 = 3 and A will be remaining in the heap.
+            // in the 2nd iteration, A will be executed, so time will be 1 only.
             if(pq.isEmpty()){
                 time+=taskCount;             
             }
