f2585b5a1c6f90433a2630c1d68143037b1fd02f
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-06-25 13:28:35 -0700

    Update searchA2DMatrix.java

--- LeetCode/searchA2DMatrix.java
+++ LeetCode/searchA2DMatrix.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m =  matrix.length;
-        int n = matrix[0].length;
+        int bottom = matrix.length-1;
+        int top = 0;
+        int row=-1;
 
-        int left = 0;
-        int right = m*n-1;
-
+        
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] <= target) {
+                row = i;
+            }
+        }
+        if(row ==-1){
+            return false;
+        }
+        
+        int left=0, right = matrix[row].length-1;
         while(left<=right){
             int mid = (left+right)/2;
 
-            int elem = matrix[mid/n][mid%n];
-
-            if(elem==target){
-                return true;
-            }
-            if(target<elem){
+            if(target<matrix[row][mid]){
                 right = mid-1;
             }
-            else{
+            else if(target>matrix[row][mid]){
                 left = mid+1;
             }
-        } 
+            else{
+                return true;
+            }
+        }
         return false;
+
     }
 }
