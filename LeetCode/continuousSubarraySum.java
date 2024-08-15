0cab14480599f24778b62526e97be3c29122f270
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-08-15 15:03:20 -0700

    Create continuousSubarraySum.java

---
+++ LeetCode/continuousSubarraySum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int prefixMod=0;
+        int prefixSum = 0;
+
+        HashMap<Integer,Integer> modSeen = new HashMap<>(); 
+        modSeen.put(0,-1);
+
+        for(int i=0;i<nums.length;i++){
+            prefixSum += nums[i];
+            prefixMod = (prefixSum)%k;
+
+            if(modSeen.containsKey(prefixMod)){
+                if(i-modSeen.get(prefixMod)>1){
+                    return true;
+                }
+            }
+            else{
+                modSeen.put(prefixMod,i);
+            }
+        }
+        return false;
+        
+
+    }
+}
