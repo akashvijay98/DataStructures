d55eb9bc7ecbe1267caee66a893ba1f1bc9932bd
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2024-08-17 17:20:54 -0700

    Create optimalPartitionOfAString.java

---
+++ LeetCode/optimalPartitionOfAString.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int partitionString(String s) {
+        int[] lastSeen = new int[26];
+
+        Arrays.fill(lastSeen,-1);
+
+        int count =1, subStringStart= 0;
+
+        for(int i=0;i<s.length();i++){
+            if(lastSeen[s.charAt(i)-'a']>=subStringStart){
+                count++;
+                subStringStart=i;
+            }
+
+            lastSeen[s.charAt(i)-'a']=i;
+        }
+
+        return count;
+
+    }
+}
