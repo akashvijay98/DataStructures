35faa404ad05439b84e570d10040aa28625dc9e7
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-05-10 10:11:07 -0700

    Update maximum_swap.java

--- LeetCode/maximum_swap.java
+++ LeetCode/maximum_swap.java
@@ -8,7 +8,9 @@ class Solution {
         int[] maxRightIndex = new int[n];
 
         maxRightIndex[n-1]=n-1;
-
+        
+        // first we compare the number at index i with number at maxRightIndex[i+1]
+        
         // maxRightIndex will store the index of maximum element from index i to till n-1
 
         for(int i=n-2;i>=0;i--){
@@ -18,6 +20,8 @@ class Solution {
             else maxRightIndex[i] =maxRightIndex[i+1]; 
         }
 
+         // we compare the number at index i and maxRightIndex[i], we swap only when number at index i is less than maxRigthIndex[i] 
+        // e.g. for number 9736 , maxRightIndex = [0,1,3,3]. so first two numbers will be skipped.
         for(int i=0;i<n;i++){
             if(nums[i]<nums[maxRightIndex[i]]){
                 char temp = nums[i];
