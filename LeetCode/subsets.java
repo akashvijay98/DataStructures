6a7cbd3809737f6d0b3e5ac0a4a57e0b6aa87305
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-05-25 19:43:55 -0700

    Update subsets.java

--- LeetCode/subsets.java
+++ LeetCode/subsets.java
@@ -3,6 +3,8 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> tempList = new ArrayList<>();
 
+         
+       // int i=0 will add empty [] to result, and i <= nums.length ---- pay attention  
         for(int k=0;k<=nums.length;k++){
             backtrack(result,tempList,k,0,nums);
         }
@@ -17,6 +19,8 @@ class Solution {
 
         for(int i=start;i<nums.length;i++){
             tempList.add(nums[i]);
+
+            // we do i+1 because we want totally unique combinations. here [1,2] and [2,1] are the same.
             backtrack(result,tempList,k,i+1,nums);
 
             tempList.remove(tempList.size()-1);
