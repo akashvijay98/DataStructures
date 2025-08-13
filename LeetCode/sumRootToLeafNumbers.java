6805f57e58df413ee59dae53158f2e5e1a9025ca
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-08-13 12:18:34 -0700

    Update sumRootToLeafNumbers.java

--- LeetCode/sumRootToLeafNumbers.java
+++ LeetCode/sumRootToLeafNumbers.java
@@ -13,31 +13,43 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int totalSum = 0;
+    int total=0;
     public int sumNumbers(TreeNode root) {
-        dfs(root,0);
-        return totalSum;
+        dfs(root, 0);
+        return total;
     }
-    public void dfs(TreeNode root,int sum){
+
+    private void dfs(TreeNode root, int sum){
         if(root == null){
             return;
         }
-        
         sum = sum*10+root.val;
-        
+
+        // check if leaf node
         if(root.left==null && root.right==null){
-            totalSum+=sum;
+            total+=sum;
         }
 
-        if(root.left !=null){
-            dfs(root.left,sum);
-        }
+        dfs(root.left,sum);
+        dfs(root.right,sum);
 
-        if(root.right !=null){
-            dfs(root.right,sum);
-        }
-
+        // dont forget to undo sum 
         sum-=root.val;
     }
 }
