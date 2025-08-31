68c4987dc7f1f37a18d1975c4ebf2a66e96d977f
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-08-30 22:04:55 -0700

    Create MinimumSubsegments.java

---
+++ AmazonOAPractise/MinimumSubsegments.java
@@ -0,0 +1,26 @@
+
+public int getminSubsegments(String frames) {
+  
+       int subsegments=1;
+
+        char type='?';
+
+        for(int i=0; i<frames.length();i+=2){
+          char c1 = frames.charAt(i);
+          char c2 = frames.charAt(i+1);
+
+          if(c1==c2){
+            if(type=='?'){
+              type = c1;
+            }
+            else if(type!=c1){
+              subsegments++;
+              type=c1;
+            }
+          }
+        }
+
+        return subsegments;
+        
+   
+}
