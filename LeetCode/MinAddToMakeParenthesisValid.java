50df78ca9190a52f245be5aafe391e83a037bc76
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2025-09-07 18:56:18 -0700

    Create MinAddToMakeParenthesisValid.java

---
+++ LeetCode/MinAddToMakeParenthesisValid.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int add = 0;
+
+        for(int i=0;i<s.length();i++){
+            char c= s.charAt(i);
+
+            if(c=='('){
+                open++;
+            }
+            else if(c=='('){
+                if(open>0){
+                    open--;
+                }
+                else{
+                    add++;
+                }
+            }
+        }
+        return open+add;
+    }
+}
