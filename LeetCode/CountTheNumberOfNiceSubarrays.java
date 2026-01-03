ef639855a0715f811bb43a23e9e56a7b81b14687
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2026-01-03 14:23:30 -0700

    Create CountTheNumberOfNiceSubarrays.java

---
+++ LeetCode/CountTheNumberOfNiceSubarrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum=0;
+        int count = 0;
+        map.put(0,1);
+
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i]%2;
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        } 
+
+        return count;
+    }
+}
