ff7d87f10abe8a8e4759cc97c70b89d926a242b8
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2026-02-13 22:10:08 -0700

    Implement maxScore method for card points problem

--- SlidingWindowScroll.java
+++ SlidingWindowScroll.java
@@ -1,1 +1,44 @@
 
+
+
+1423. Maximum Points You Can Obtain from Cards
+
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int windowSum =0;
+
+
+        int max=0;
+
+
+        int start=0;
+
+
+        int n = cardPoints.length;
+
+
+       // initially we take the sum of last k cards by iterating from n-k to n. 
+       for(int i = n-k; i<cardPoints.length;i++){
+            windowSum+=cardPoints[i];
+        }
+
+
+        max = Math.max(max, windowSum);
+
+
+        int r = n-k;
+        while(r<cardPoints.length){
+            windowSum = windowSum-cardPoints[r] + cardPoints[start];
+            start++;
+            r++;
+
+
+            max= Math.max(max, windowSum);
+        }
+
+
+        return max;
+    }
+}
+
