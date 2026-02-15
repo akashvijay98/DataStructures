8445d00ad3c9438745fe20ca79107742835bf37c
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2026-02-14 20:33:46 -0700

    Update BinaryTreeScroll.java

--- BinaryTreeScroll.java
+++ BinaryTreeScroll.java
@@ -26,3 +26,80 @@ class Solution {
 
     }
 }
+
+
+// 543. Diameter of Binary Tree
+
+class Solution {
+
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        max = Math.max(max, left+right);
+
+        return 1+Math.max(left,right);
+    }
+}
+
+
+// Kth Smallest element in a BST
+class Solution {
+    int val;
+    int count=0;
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root,k);
+        return val;
+    }
+
+    private void dfs(TreeNode root, int k){
+        if(root==null){
+            return;
+        }
+
+        dfs(root.left,k);
+        
+        count++;
+        
+        if(count==k){
+            val = root.val; 
+            return;  
+        }
+
+        dfs(root.right,k);
+    }
+}
+
+
+//98. Validate Binary Search Tree
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean dfs(TreeNode root, int left, int right){
+        if(root==null){
+            return true;
+        }
+
+        if(root.val>right || root.val<left){
+            return false;
+        }
+
+        return dfs(root.left,left,root.val) && dfs(root.right,root.val,right);
+    }
+
+   
+}
