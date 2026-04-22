9113e6d798eebf8e42c87d23cda9a2fbdf2e5765
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2026-04-22 16:36:57 -0700

    Create HitCounter.java

---
+++ LeetCode/HitCounter.java
@@ -0,0 +1,26 @@
+// 362. Design Hit Counter
+class HitCounter {
+    Queue<Integer> q;
+    public HitCounter() {
+        q= new LinkedList<>();
+    }
+    
+    public void hit(int timestamp) {
+        q.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        while(!q.isEmpty() && (timestamp-q.peek()) >= 300){
+            q.poll();
+        }
+
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
