24cb4081a4b491621c232d470286927a9cbfa63a
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2026-04-26 22:35:29 -0700

    Update mergeIntervals.java

--- LeetCode/mergeIntervals.java
+++ LeetCode/mergeIntervals.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+        List<int[]> list =  new ArrayList<>();
+
+        int i;
+
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
 
         int start = intervals[0][0];
         int end = intervals[0][1];
 
-        List<int[]> res = new ArrayList<>();
+        for(i=1; i<intervals.length;i++){
+            if(intervals[i][0]>end){
+                int[] arr = new int[]{start, end};
+                list.add(arr);
 
-        for(int i=1;i<intervals.length;i++){
-            int newStart = intervals[i][0];
-            int newEnd = intervals[i][1];
-
-            if(end>= newStart){
-                end = Math.max(end,newEnd); 
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
-            else{
-                res.add(new int[]{start, end});
 
-                start= newStart;
-                end= newEnd;   
+            else{
+                end = Math.max(end, intervals[i][1]);
             }
         }
-           res.add(new int[]{start, end});
 
-        return res.toArray(new int[res.size()][]);
+       
+        list.add(new int[]{start,end});
+        
+        return list.toArray(new int[list.size()][]);
     }
 }
